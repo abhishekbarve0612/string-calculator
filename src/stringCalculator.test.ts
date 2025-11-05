17bf61fa4bf7755da8957f7042e22223bf6b8daf
Abhishek Barve <barveabhishek.ab@gmail.com>
2025-11-05 08:37:08 +0530

    Add calculateNumbers function to string calculator and update tests for operator handling

--- src/stringCalculator.test.ts
+++ src/stringCalculator.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest'
-import { calculate, getOperandsAndOperator } from './stringCalculator'
+import { calculate, calculateNumbers, getOperandsAndOperator, type Operator } from './stringCalculator'
 
 describe('getOperandsAndOperator helper function', () => {
     it('returns the operands and operator if an operator is present', () => {
@@ -9,6 +9,21 @@ describe('getOperandsAndOperator helper function', () => {
         expect(getOperandsAndOperator('1/2')).toEqual([1, 2, '/'])
         expect(getOperandsAndOperator('1%2')).toEqual([1, 2, '%'])
         expect(getOperandsAndOperator('1^2')).toEqual([1, 2, '^'])
+    })
+})
+
+describe('calculateNumbers helper function', () => {
+    it('returns the calculated result if an operator is present', () => {
+        expect(calculateNumbers(1, 2, '+')).toBe(3)
+        expect(calculateNumbers(1, 2, '-')).toBe(-1)
+        expect(calculateNumbers(1, 2, '*')).toBe(2)
+        expect(calculateNumbers(1, 2, '/')).toBe(0.5)
+        expect(calculateNumbers(1, 2, '%')).toBe(1)
+        expect(calculateNumbers(2, 3, '^')).toBe(8)
+    })
+
+    it('throws an error if an invalid operator is present', () => {
+        expect(() => calculateNumbers(1, 2, 'a' as Operator)).toThrow('Invalid operator: a')
     })
 })
 
--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,13 +1,34 @@
 const OPERATORS = /[+\-*/%^]/
+export type Operator = '+' | '-' | '*' | '/' | '%' | '^' | ''
 
-export function getOperandsAndOperator(input: string): [operand1: number, operand2: number, operator: string] {
+export function getOperandsAndOperator(input: string): [operand1: number, operand2: number, operator: Operator] {
     const trimmedInput = input.trim()
     if (!OPERATORS.test(trimmedInput)) {
         return [Number(trimmedInput), 0, '']
     }
     const operands = trimmedInput.split(OPERATORS)
-    const operator = trimmedInput.match(OPERATORS)?.[0] ?? ''
+    const operator = (trimmedInput.match(OPERATORS)?.[0] ?? '') as Operator
     return [Number(operands[0]), Number(operands[1]), operator]
+}
+
+
+export function calculateNumbers(operand1: number, operand2: number, operator: Operator): number {
+    switch (operator) {
+        case '+':
+            return operand1 + operand2
+        case '-':
+            return operand1 - operand2
+        case '*':
+            return operand1 * operand2
+        case '/':
+            return operand1 / operand2
+        case '%':
+            return operand1 % operand2
+        case '^':
+            return operand1 ** operand2
+        default:
+            throw new Error(`Invalid operator: ${operator}`)
+    }
 }
 
 export function calculate(input: string): number {
